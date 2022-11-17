ff8905f4c649518294f26cb6ba20abff3fd39570
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2022-11-18 00:37:10 +0900

    作業の一時保存：OrmTxcSql.Attributes名前空間のクラスを移植。

---
+++ Sources.Net6/OrmTxcSql/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OrmTxcSql.Attributes
+{
+    /// <summary>
+    /// 主キー属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+        /// <summary>
+        /// コンストラクタ.
+        /// </summary>
+        public PrimaryKeyAttribute()
+        {
+        }
+    }
+}
---
+++ Sources.Net6/OrmTxcSql/Attributes/UIDAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrmTxcSql.Attributes
+{
+    /// <summary>
+    /// UID（unique identifier）属性
+    /// </summary>
+    /// <remarks>
+    /// 値の設定（採番を含む）はDBMS側の機能で実施し、プログラム側では参照のみのカラムに設定する。
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class UIDAttribute : Attribute
+    {
+        /// <summary>
+        /// コンストラクタ.
+        /// </summary>
+        public UIDAttribute()
+        {
+        }
+    }
+}
