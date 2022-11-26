29f8b2e965f9e5376ddf988202fb1b3a0d6b0b6d
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2022-11-27 02:30:08 +0900

    作業の一時保存：OrmTxcSql.Data.DataSourceを更新。

--- Sources.Net6/OrmTxcSql/Data/DataSource.cs
+++ Sources.Net6/OrmTxcSql/Data/DataSource.cs
@@ -12,26 +12,28 @@ namespace OrmTxcSql.Data
     /// </remarks>
     public abstract class DataSource
     {
-
         /// <summary>
         /// 接続文字列を取得します。
         /// </summary>
         /// <returns></returns>
         /// <remarks>
-        /// 接続文字列を戻す処理の実装には何パターンか考えられる。
-        /// １．固定値を戻す。
-        /// ２．選択肢をプロパティで公開し、プロパティに応じた接続文字列を戻す。
-        /// ３．接続文字列をプロパティで公開し、プロパティの値を戻す。
+        /// 接続文字列を戻す処理の実装には何パターンか考えられる。<br/>
+        /// １．固定値を戻す。<br/>
+        /// ２．選択肢をプロパティで公開し、プロパティに応じた接続文字列を戻す。<br/>
+        /// ３．接続文字列をプロパティで公開し、プロパティの値を戻す。<br/>
         /// </remarks>
         public abstract string GetConnectionString();
 
-        ///// <summary>
-        ///// <see cref="RemoteCertificateValidationCallback"/>を取得します。
-        ///// </summary>
-        ///// <returns></returns>
-        //public virtual RemoteCertificateValidationCallback GetRemoteCertificateValidationCallback()
-        //{
-        //    return null;
-        //}
+        /// <summary>
+        /// <see cref="RemoteCertificateValidationCallback"/>を取得します。
+        /// </summary>
+        /// <returns></returns>
+#if NET6_0_OR_GREATER
+        [return: MaybeNull]
+#endif
+        public virtual RemoteCertificateValidationCallback GetRemoteCertificateValidationCallback()
+        {
+            return null;
+        }
     }
 }
