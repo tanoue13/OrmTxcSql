c107999e0af3878757ba0c035a75becc0eeae3f3
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2023-01-09 14:27:37 +0900

    作業の一時保存：テストケース用のクラスを作成。

---
+++ NpgsqlSample01/Daos/AutoNumberingDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NpgsqlSample01.Entities;
+
+namespace NpgsqlSample01.Daos
+{
+    public class AutoNumberingDao : BaseNpgsqlDao<AutoNumberingEntity>
+    {
+        public override AutoNumberingEntity[] Select(AutoNumberingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NpgsqlSample01/Daos/NoPrimaryKeyDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NpgsqlSample01.Entities;
+
+namespace NpgsqlSample01.Daos
+{
+    public class NoPrimaryKeyDao : BaseNpgsqlDao<NoPrimaryKeyEntity>
+    {
+        public override NoPrimaryKeyEntity[] Select(NoPrimaryKeyEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
