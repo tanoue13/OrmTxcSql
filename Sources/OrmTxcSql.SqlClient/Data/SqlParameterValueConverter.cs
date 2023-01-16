17aec058bd44de81f54406ec8851c082dc2c5a88
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2023-01-16 14:22:13 +0900

    作業の一時保存：Data名前空間のクラスを移植。

---
+++ Sources/OrmTxcSql.SqlClient/Data/SqlParameterValueConverter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using OrmTxcSql.Data;
+
+namespace OrmTxcSql.SqlClient.Data
+{
+    /// <summary>
+    /// SqlParameterの値に変換するコンバータ。
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    public class SqlParameterValueConverter : IParameterValueConverter
+    {
+        /// <summary>
+        /// 変換する。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+#if NET6_0_OR_GREATER
+        public object Convert(object? value, Type? targetType, object? parameter)
+#else
+        public object Convert(object value, Type targetType, object parameter)
+#endif
+        {
+            if (null == value)
+            {
+                // nullの場合、DBNullを戻す。
+                return DBNull.Value;
+            }
+            //
+            Type valueType = value.GetType();
+            if (typeof(DBNull).Equals(valueType))
+            {
+                // DBNullを戻す。
+                return DBNull.Value;
+            }
+            else if (typeof(string).Equals(valueType))
+            {
+                // string型を変換する。
+                string sValue = value as string;
+                if (null != sValue)
+                {
+                    return value;
+                }
+                else
+                {
+                    return DBNull.Value;
+                }
+            }
+            else if (typeof(int?).Equals(valueType))
+            {
+                // int型を変換する。
+                int? iValue = value as int?;
+                if (iValue.HasValue)
+                {
+                    return value;
+                }
+                else
+                {
+                    return DBNull.Value;
+                }
+            }
+            else if (typeof(decimal?).Equals(valueType))
+            {
+                // int型を変換する。
+                decimal? dValue = value as decimal?;
+                if (dValue.HasValue)
+                {
+                    return value;
+                }
+                else
+                {
+                    return DBNull.Value;
+                }
+            }
+            else if (typeof(DateTime?).Equals(valueType))
+            {
+                // DateTime型を変換する。
+                DateTime? dValue = value as DateTime?;
+                if (dValue.HasValue)
+                {
+                    return this.SpecifyKindLocalIfUnspecified(dValue.Value);
+                }
+                else
+                {
+                    return DBNull.Value;
+                }
+            }
+            else if (typeof(DateTime).Equals(valueType))
+            {
+                // DateTime型を変換する。
+                if (value is DateTime dValue)
+                {
+                    return this.SpecifyKindLocalIfUnspecified(dValue);
+                }
+                else
+                {
+                    return value;
+                }
+            }
+            // fool-proof
+            return value;
+        }
+        /// <summary>
+        /// DateTime.KindがUnspecifiedの場合、
+        /// 指定された DateTime と同じティック数、および、Kind値がLocalを持つ新しい DateTime オブジェクトを戻す。
+        /// </summary>
+        /// <param name="value">DateTimeオブジェクト</param>
+        /// <returns></returns>
+        private DateTime SpecifyKindLocalIfUnspecified(DateTime value)
+        {
+            if (DateTimeKind.Unspecified == value.Kind)
+            {
+                DateTime dateTimeLocal = DateTime.SpecifyKind(value, DateTimeKind.Local);
+                return dateTimeLocal;
+            }
+            //
+            // fool-proof
+            return value;
+        }
+    }
+}
