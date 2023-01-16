3866536c6e70de68179ecab8f70b44e94f936941
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2023-01-16 15:48:34 +0900

    作業の一時保存：サンプル用クラスを追加。

---
+++ Samples/SqlSample01/Data/SqlDataSource.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+using OrmTxcSql.Data;
+
+namespace SqlSample01.Data
+{
+    public class SqlDataSource : DataSource
+    {   /// <summary>
+        /// 接続文字列ビルダ
+        /// </summary>
+        private readonly SqlConnectionStringBuilder _connectionStringBuilder;
+
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public SqlDataSource()
+        {
+            // 接続文字列ビルダを生成する。
+            _connectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = "localhost",
+                InitialCatalog = "sample01",
+                IntegratedSecurity = true,
+                ApplicationName = this.GetType().FullName,
+            };
+        }
+
+        public override string GetConnectionString()
+        {
+            string connectionString = _connectionStringBuilder.ToString();
+            return connectionString;
+        }
+    }
+}
