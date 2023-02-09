aa0d0ca25f5fbd9a9b60ea17d4b44fff471331c1
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2023-02-09 10:59:29 +0900

    作業の一時保存：Daos名前空間のクラスを移植。

--- Sources/OrmTxcSql.DB2/Entities/DB2Entity.cs
+++ Sources/OrmTxcSql.DB2/Entities/DB2Entity.cs
@@ -8,5 +8,24 @@ namespace OrmTxcSql.DB2.Entities
     public abstract class DB2Entity : AbstractEntity
     {
 
+        /// <summary>
+        /// テーブル内の相対行番号（Relative Record Number）を取得する際のカラム名です。
+        /// </summary>
+        internal static string RRNColumnName
+        {
+            get => DB2Entity.s_rrnColumnName;
+        }
+        private readonly static string s_rrnColumnName = "relative_record_number";
+
+        /// <summary>
+        /// テーブル内の相対行番号（Relative Record Number）を取得または設定します。
+        /// </summary>
+        public int? RelativeRecordNumber
+        {
+            get => this._relativeRecordNumber;
+            set => this._relativeRecordNumber = value;
+        }
+        private int? _relativeRecordNumber;
+
     }
 }
