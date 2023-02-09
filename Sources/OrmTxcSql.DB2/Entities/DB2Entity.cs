7c5df6160ca382b6e2aec73ebbd601e72df54321
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2023-02-09 10:51:49 +0900

    作業の一時保存：Entities名前空間のクラスを移植。

---
+++ Sources/OrmTxcSql.DB2/Entities/DB2Entity.cs
@@ -0,0 +1,12 @@
+﻿using OrmTxcSql.Entities;
+
+namespace OrmTxcSql.DB2.Entities
+{
+    /// <summary>
+    /// DB2用のエンティティクラス。
+    /// </summary>
+    public abstract class DB2Entity : AbstractEntity
+    {
+
+    }
+}
