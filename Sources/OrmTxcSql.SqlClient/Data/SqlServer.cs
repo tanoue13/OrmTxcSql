b4d57ed4af89fbe1cbd329dab6543d6b853ec58e
田上 雅明 <tanoue.13.masaaki@jp.nokgrp.com>
2024-09-16 10:19:45 +0900

    improve(OrmTxcSql): 基底パッケージにDbServer<TConnection>.ExecuteAsync() を追加

--- Sources/OrmTxcSql.SqlClient/Data/SqlServer.cs
+++ Sources/OrmTxcSql.SqlClient/Data/SqlServer.cs
@@ -294,7 +294,7 @@ namespace OrmTxcSql.SqlClient.Data
         /// <param name="daos">トランザクションに参加する<see cref="IDao"/>のコレクション</param>
         /// <param name="action">トランザクション管理下で実行される処理</param>
         [Obsolete("プロバイダーによる実装ではないため、通常の Execute(IEnumerable<IDao> daos, Action<IDbTransaction> action) を使用ください。")]
-        public async ValueTask ExecuteAsync(IEnumerable<IDao> daos, Action<IDbTransaction> action)
+        public override async ValueTask ExecuteAsync(IEnumerable<IDao> daos, Action<IDbTransaction> action)
         {
             using (var connection = new SqlConnection())
             {
