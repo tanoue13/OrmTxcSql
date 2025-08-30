f7689e7c9c7e994d0609fac9714b5c9764602461
田上 雅明 <masaaki.tanoue@gmail.com>
2025-08-30 10:17:57 +0900

    test(OrmTxcSql.Npgsql): DeleteByPk のテストケースを追加

--- Tests/NpgsqlSample01Tests/Daos/ProductDaoTest.cs
+++ Tests/NpgsqlSample01Tests/Daos/ProductDaoTest.cs
@@ -6,6 +6,8 @@ using NpgsqlSample01.Entities;
 using OrmTxcSql.Daos;
 using OrmTxcSql.Npgsql.Data;
 using OrmTxcSql.Tests.Utils;
+using System.Data;
+
 #if NET462 || NET48
 using NUnit.Framework;
 #endif
@@ -144,6 +146,57 @@ namespace OrmTxcSql.Tests.NpgsqlSample01Tests.Daos
                 //
                 var result01 = dao.Select(new ProductEntity());
                 DebugUtils.Dump<ProductEntity>(result01);
+                //
+                // ロールバックする。
+                tx.Rollback();
+            });
+        }
+
+        [Test]
+        public void DeleteByPkTest_E01()
+        {
+            var entityI0 = _entities[0];
+            var entityI1 = _entities[1];
+            //
+            var dao = new ProductDao();
+            //
+            var server = new NpgsqlServer();
+            server.DataSource = new NpgsqlSample01.Data.NpgsqlDataSource();
+            server.Execute(new IDao[] { dao }, tx =>
+            {
+                foreach (ProductEntity entity in _entities)
+                {
+                    dao.Insert(entity);
+                }
+                //
+                var result00 = dao.Select(new ProductEntity());
+                DebugUtils.Dump<ProductEntity>(result00);
+                //
+                var entityU0 = result00.First();
+                entityU0.Description = entityU0.Description?.ToUpperInvariant();
+                //
+                dao.UpdateByPk(entityU0);
+                var result01 = dao.Select(new ProductEntity());
+                DebugUtils.Dump<ProductEntity>(result01);
+                //
+                Assert.Throws<DBConcurrencyException>(() =>
+                {
+                    // 主キーで削除する：
+                    // →versionNo の相違により、更新対象が０健となり、 DBConcurrencyException が発生する。
+                    dao.DeleteByPk(entityU0);
+                    // ２回目の削除は、主キーに該当する行が存在しないため、DBConcurrencyExceptionが発生する。
+                    //                    dao.DeleteByPk(entityU0);
+                });
+                //
+                entityU0.VersionNo = 1;
+                dao.DeleteByPk(entityU0);
+                Assert.Throws<DBConcurrencyException>(() =>
+                {
+                    // 再度の削除する：
+                    // →主キーに該当する行が存在しないため、DBConcurrencyExceptionが発生する。
+                    dao.DeleteByPk(entityU0);
+                });
+                //
                 //
                 // ロールバックする。
                 tx.Rollback();
